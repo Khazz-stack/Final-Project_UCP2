7704d259fd08e9f5dae2f74bc17401892f9a0aa5
Khazz <thelenaid@gmail.com>
2026-08-24 22:56:15 +0700

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/api'); // sesuaikan dengan file route Anda
+const routes = require('./routes/api');
 
 app.use(express.json());
 app.use('/api', routes);
@@ -8,6 +8,14 @@ app.use('/api', routes);
 app.get('/', (req, res) => {
   res.json({ message: 'Backend Server is Running!' });
 });
+
+// BERJALAN DI LOKAL (Hanya dipanggil jika dijalankan lewat 'npm start' / 'node index.js')
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`);
+  });
+}
 
 // WAJIB UNTUK VERCEL SERVERLESS:
 module.exports = app;
